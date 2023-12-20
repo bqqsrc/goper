27ad502194b313f4788e2f6b7e5393a15a49fed3
WangXiaojie01 <1259488569@qq.com>
2023-12-20 09:55:05 +0800

    删除旧版本的代码

--- error_msg.go
+++
@@ -1,7 +0,0 @@
-package goper
-
-var errorMsg = map[int]string{
-	1000: "read body error, ioutil.ReadAll error, err is %s",
-	1001: "parse body error, json.Unmarshal error, err is %s",
-	1002: "parse rawQuery error, url.QueryUnescape error, err is %s",
-}
